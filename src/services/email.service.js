ecc1d053f480d922199d8016cb53f6006e59a159
Mihir Panchal <pmihir98512@gmail.com>
2021-11-29 13:06:38 +0530

    json removed

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import userEmails from "../data/userEmails.json";
+// import userEmails from "../data/userEmails.json";
 
 export const NotifyEmail = async (
   department,
